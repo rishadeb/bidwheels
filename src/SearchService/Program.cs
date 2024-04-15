4087f6732888bdee71cd87c97e24cc8232ccb1ed
rishadeb <rishadm.ebrahim@gmail.com>
2024-04-15 22:04:51 +0200

    add mongodb connection and seed data

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +14,15 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
 
