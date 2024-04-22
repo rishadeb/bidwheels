b83e3e988c4db476b98fcbdab64aaa6298099c79
rishadeb <rishadm.ebrahim@gmail.com>
2024-04-22 21:40:16 +0200

    add message outbox for message broker and retry functionality for mongodb

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -20,6 +20,14 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) => 
     {
+        // configure retry policy per endpoint
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
         cfg.ConfigureEndpoints(context);
     });
 });
