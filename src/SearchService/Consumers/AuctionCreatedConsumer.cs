7b2a385f5991e2b446d170d997b180cbd035973a
rishadeb <rishadm.ebrahim@gmail.com>
2024-04-22 22:00:23 +0200

    add example of handling exceptions using message broker

--- src/SearchService/Consumers/AuctionCreatedConsumer.cs
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -18,6 +18,9 @@ public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
     {
         Console.WriteLine("--> Consuming auction created: " + context.Message.Id);
         var item = _mapper.Map<Item>(context.Message);
+
+        if (item.Model == "Foo") throw new ArgumentException("Cannot sell cars with name of Foo");
+
         await item.SaveAsync();
     }
 }
