3da6b5a177cc3401713e4a172b9777e3bc2b9679
rishadeb <rishadm.ebrahim@gmail.com>
2024-05-23 21:04:51 +0200

    add consumers to search service and add config for client app to Identity service

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -28,6 +28,18 @@ public static class Config
                 RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
                 ClientSecrets = new [] {new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials, // token used by server side not browser client
+                RequirePkce = false,
+                RedirectUris = {"https://localhost:3000/api/auth/callback/id-server"},
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600*24*30// not recommended for security -- too long time
             }
         };
 }
