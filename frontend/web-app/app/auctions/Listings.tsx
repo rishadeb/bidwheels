85052d66081024b0011a325733e97ef83642f284
rishadeb <rishadm.ebrahim@gmail.com>
2024-05-24 17:21:49 +0200

    add nextjs front end app

---
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import AuctionCard from './AuctionCard';
+import { Auction, PagedResult } from '@/types';
+import AppPagination from '../components/AppPagination';
+
+async function getData() : Promise<PagedResult<Auction>> {
+    const res = await fetch('http://localhost:6001/search?pageSize=4');
+
+    if (!res.ok) throw new Error('Failed to fetch data');
+    return res.json();
+}
+
+export default async function Listings() {
+    const data = await getData();
+
+  return (
+    <>
+      <div className='grid grid-cols-4 gap-6 '>
+          {data && data.results.map((auction) => (
+              <AuctionCard auction={auction} key={auction.id}/>
+          ))}
+      </div>
+      <div className='flex justify-center mt-4' >
+        <AppPagination currentPage={1} pageCount={data.pageCount}/>
+      </div>
+    </>
+  )
+}
