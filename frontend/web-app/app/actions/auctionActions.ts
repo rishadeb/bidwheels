7f195ae9f72e9cbe4cfa75cce18d5ea4c823b635
rishadeb <rishadm.ebrahim@gmail.com>
2024-05-28 15:41:09 +0200

    add auction detail pages and update/delete functionality

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -4,6 +4,7 @@ import { Auction, PagedResult } from "@/types";
 import { getTokenWorkaround } from "./authActions";
 import { fetchWrapper } from "@/lib/fetchWrapper";
 import { FieldValues } from "react-hook-form";
+import { revalidatePath } from "next/cache";
 
 export async function getData(query: string) : Promise<PagedResult<Auction>> {
     return await fetchWrapper.get(`search${query}`);
@@ -18,4 +19,18 @@ export async function updateAuctionTest() {
 
 export async function createAuction(data: FieldValues) {
     return await fetchWrapper.post('auctions', data);
+}
+
+export async function updateAuction(data: FieldValues, id: string) {
+    const res = await fetchWrapper.put(`auctions/${id}`, data);
+    revalidatePath(`/auctions/${id}`);
+    return res;
+}
+
+export async function deleteAuction(id: string) {
+    return await fetchWrapper.del(`auctions/${id}`);
+}
+
+export async function getDetailedViewData(id : string): Promise<Auction> {
+    return await fetchWrapper.get(`auctions/${id}`);
 }
