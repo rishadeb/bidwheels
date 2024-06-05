e0ec7601a1f0bb122e9c0d9554c784f549008303
rishadeb <rishadm.ebrahim@gmail.com>
2024-06-05 23:28:41 +0200

    add ssl

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -21,14 +21,19 @@ export default function SignalRProvider({children, user }: Props) {
     const [connection, setConnection] = useState<HubConnection | null>(null);
     const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
     const addBid = useBidStore(state => state.addBid);
+    // workaround for public variables in docker
+    // not ideal but works for local deployment
+    const apiUrl = process.env.NODE_ENV  === 'production'
+        ? 'https://api.bidwheels.com/notifications'
+        :  process.env.NEXT_PUBLIC_NOTIFY_URL
 
     useEffect(() => {
         const newConnection = new HubConnectionBuilder()
-            .withUrl(process.env.NEXT_PUBLIC_NOTIFY_URL!)
+            .withUrl(apiUrl!)
             .withAutomaticReconnect()
             .build();
         setConnection(newConnection);
-        }, [])
+        }, [apiUrl])
     
     useEffect(() => {
         if (connection) {
