0dba40458c63bb7ec8c93dbad6e62b5959604973
rishadeb <rishadm.ebrahim@gmail.com>
2025-01-17 13:55:22 +0200

    add repository pattern and prepare for unit tests

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -46,6 +46,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 builder.Services.AddGrpc();
+builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
 
 var app = builder.Build();
 
